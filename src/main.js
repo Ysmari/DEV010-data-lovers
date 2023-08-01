81ed1bd9cd926f2d463abb5260a0bdb330d9d54d
DianaJ-Dev <134237911+DianaJ-Dev@users.noreply.github.com>
2023-08-01 12:15:04 -0500

    material icon

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/countries/countries.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+const root = document.getElementById('root');
+
+let contentRoot = '';
+for (let i= 0; i<data.countries.length; i++) {  
+    if(data.countries[i].subregion === "South America") {
+        contentRoot += `<img src="${data.countries[i].flags.png}">`;    
+    }
+}
+root.innerHTML=contentRoot;
+
+
