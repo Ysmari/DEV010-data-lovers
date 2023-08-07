3fb4ad4167c948dfb4310e239e329f4ed109fcea
DianaJ-Dev <134237911+DianaJ-Dev@users.noreply.github.com>
2023-08-07 18:30:06 -0500

    Funcion de bandera y boton de filtros

--- src/main.js
+++ src/main.js
@@ -3,53 +3,68 @@ import data from './data/countries/countries.js';
 
 console.log(example, data);
 
-// me trae de la data nombre y banderas
 const root = document.getElementById('root');
-let contentRoot = '';
-for (let i= 0; i<data.countries.length; i++) {  
-    if(data.countries[i].subregion === "South America") { // selecciono solo sur america
-       contentRoot += `<div class="BanderaContenedor">
-        <img src="${data.countries[i].flags.png}" class="Bandera" 
-        <h4>${data.countries[i].name.common}</h4> 
-       </div>`;   
-    }} // 
-root.innerHTML=contentRoot;
 
+//movimiento de las banderas 
 
-// Cambiar el color de los botones automáticamente cada 2 segundos
-const buttons = document.querySelectorAll('anside button'); 
-function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+let contentRootInfo = '';
+for (let i = 0; i < data.countries.length; i++) {
+  if (data.countries[i].subregion === 'South America') {
+      const country = data.countries[i];
+      const languages = Object.values(country.languages).join(', ');
+
+      contentRootInfo += `<div class="bandera-container">
+          <div class="bandera-wrapper">
+              <img src="${country.flags.png}" class="Bandera" id="bandera-${i}">
+              <h4>${country.name.common}</h4> 
+          </div>
+          <div class="info-back">
+              <h4>${country.name.common}</h4>
+              <p>Población: ${country.population}</p>
+              <p>Área: ${country.area}</p>
+              <p>Países Frontera: ${country.borders}</p>
+              <p>Idiomas: ${languages}</p>
+          </div>
+      </div>`;
   }
-  return color;}
-function changeButtonColor() {
-  buttons.forEach(button => {
-    const randomColor = getRandomColor();
-    button.style.backgroundColor = randomColor;
-  });}
-setInterval(changeButtonColor, 2000)
+}
 
-//lista de boton de filtros
+root.innerHTML=contentRootInfo;
+
+const banderaImages = document.querySelectorAll('.Bandera');
+
+banderaImages.forEach(image => {
+  const container = image.closest('.bandera-container');
+  const infoBack = container.querySelector('.info-back');
+  
+  image.addEventListener('click', function () {
+    container.classList.toggle('show-info');
+    image.classList.toggle('rotate');
+  });
+
+  infoBack.addEventListener('click', function () {
+    container.classList.remove('show-info');
+    image.classList.remove('rotate');
+  });
+});
+
+//Boton de filtros paises limitantes.
 
 // Debemos llamar las funciones que vamos a uctilizar 
-const filtroForm= document.getElementById('filtroForm'); // El formulario
 const  btnLimites = document.getElementById('limitantes'); // El boton 
 
 btnLimites.addEventListener("click", function () { // Activo la funcion del botones
-  const inputPais = document.getElementById('nombrePais'); // Traigo el inpu que se creo para colocar el nombre 
+  const inputPais = document.getElementById('input'); // Traigo el inpu que se creo para colocar el nombre 
   let banderasLimitantes = ''; // Variable con cadena de texto vacia 
   for (let i= 0; i<data.countries.length; i++) { // Este for es para que me busque la palabra que coloque en la data
-    if(data.countries[i].name.common.toLowerCase()== inputPais.value) { //Se hace el recorrido cuando sea el mismo nombre lo debe traer 
+    if(data.countries[i].name.common== inputPais.value) { //Se hace el recorrido cuando sea el mismo nombre lo debe traer 
       let borders = data.countries[i].borders // Creo una variable que sea igual a la data segun lo que se requiera 
-      
+
       
       for (let j= 0; j<borders.length; j++) {  // Este for me trae los bordes paises limites de cada pais 
         for (let k= 0; k<data.countries.length; k++) {  // Este for me trae la imagen de la badera con el nombre 
           if(borders[j] === data.countries[k].fifa) { // si los bordes son iguales a lo que tengo en la data me traiga imagenes
-             banderasLimitantes += `<div class="BanderaContenedor"> 
+              banderasLimitantes += `<div class="bandera-wrapper"> 
               <img src="${data.countries[k].flags.png}" class="Bandera" 
               <h4>${data.countries[k].name.common}</h4> 
              </div>`;   
@@ -60,6 +75,34 @@ btnLimites.addEventListener("click", function () { // Activo la funcion del boto
   }
   root.innerHTML = banderasLimitantes;
   });
+
+// Boton de filtro Idioma 
+  const btnIdioma = document.getElementById('idioma')
+  let banderasByIdioma = '';
+
+  btnIdioma.addEventListener('click',function () {
+    const inputIdioma = document.getElementById('input');
+    for (let i=0; i<data.countries.length; i++){
+      let lenguajes = data.countries[i].languages;
+      for(const key in lenguajes) {
+        if(lenguajes[key]=== inputIdioma.value) {
+          banderasByIdioma += `<div class="bandera-wrapper"> 
+              <img src="${data.countries[i].flags.png}" class="Bandera" 
+              <h4>${data.countries[i].name.common}</h4> 
+             </div>`;
+        }
+      }    
+    }
+    root.innerHTML = banderasByIdioma;
+  })
+
+
+  
+
+
+
+
+
 
 
 
