cf0270a9a90849c45704e71e68b950f2b9929937
DianaJ-Dev <134237911+DianaJ-Dev@users.noreply.github.com>
2023-08-07 00:35:34 -0500

    btnLimites

--- src/main.js
+++ src/main.js
@@ -30,22 +30,36 @@ function changeButtonColor() {
     const randomColor = getRandomColor();
     button.style.backgroundColor = randomColor;
   });}
-setInterval(changeButtonColor, 2000);
+setInterval(changeButtonColor, 2000)
 
 //lista de boton de filtros
 
+// Debemos llamar las funciones que vamos a uctilizar 
+const filtroForm= document.getElementById('filtroForm'); // El formulario
+const  btnLimites = document.getElementById('limitantes'); // El boton 
 
-
-
-
-
-
-
-
-
-
-
-
+btnLimites.addEventListener("click", function () { // Activo la funcion del botones
+  const inputPais = document.getElementById('nombrePais'); // Traigo el inpu que se creo para colocar el nombre 
+  let banderasLimitantes = ''; // Variable con cadena de texto vacia 
+  for (let i= 0; i<data.countries.length; i++) { // Este for es para que me busque la palabra que coloque en la data
+    if(data.countries[i].name.common.toLowerCase()== inputPais.value) { //Se hace el recorrido cuando sea el mismo nombre lo debe traer 
+      let borders = data.countries[i].borders // Creo una variable que sea igual a la data segun lo que se requiera 
+      
+      
+      for (let j= 0; j<borders.length; j++) {  // Este for me trae los bordes paises limites de cada pais 
+        for (let k= 0; k<data.countries.length; k++) {  // Este for me trae la imagen de la badera con el nombre 
+          if(borders[j] === data.countries[k].fifa) { // si los bordes son iguales a lo que tengo en la data me traiga imagenes
+             banderasLimitantes += `<div class="BanderaContenedor"> 
+              <img src="${data.countries[k].flags.png}" class="Bandera" 
+              <h4>${data.countries[k].name.common}</h4> 
+             </div>`;   
+          }
+        }
+      }
+    }
+  }
+  root.innerHTML = banderasLimitantes;
+  });
 
 
 
