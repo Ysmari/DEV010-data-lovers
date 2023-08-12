02096dfdd3352cdfc35d5ff43a3ae5eefcf49cd8
DianaJ-Dev <134237911+DianaJ-Dev@users.noreply.github.com>
2023-08-12 12:25:23 -0500

    Oh gene (#12)
    * modularizando filtro por idioma
    
    * Idioma  datajs-mainjs
    
    * Agregue boton calcular

--- src/data.js
+++ src/data.js
@@ -8,4 +8,62 @@ export const anotherExample = () => {
   return 'OMG';
 };
 
+// Data por Idioma
+export const filtrarIdioma = (data, inputIdioma) =>{
+  const dataFiltradaIdioma = [];
+  for (let i=0; i<data.length; i++){
+    let lenguajes = data[i].languages;
+    for(const key in lenguajes) {
+      if(lenguajes[key].toLowerCase()=== inputIdioma.toLowerCase()) {
+        dataFiltradaIdioma.push(data[i])
+       
+      }
+    }    
+  }
+  return dataFiltradaIdioma;
+}
+
+// Calcula Area entre dos paises
+export const calcularArea = (data, inpuCalcular1,inpuCalcular2) =>{
+  const pais1 = data.countries.find(pais => pais.name.common === inpuCalcular1); //se busca el pais en la data ingresado
+  const pais2= data.countries.find(pais => pais.name.common === inpuCalcular2);
+
+  if (pais1 != null && pais2 != null) {
+    return pais1.area + pais2.area;
+  }
+  else if (pais1===pais2){
+    return pais1.area
+  }
+  else {
+    alert('No existe alguno de los paises')
+  }
+};
+
+
+
+export const ordenarAZ = (banderas) => {
+  const banderasClonadas = [...banderas]; // Clona las banderas existentes
+   banderasClonadas.sort((a, b) => { //// Ordena las banderas clonadas por nombre de A a Z
+    const nombreA = a.querySelector('h4').textContent;
+    const nombreB = b.querySelector('h4').textContent;
+    return nombreA.localeCompare(nombreB);
+  });
+
+  return banderasClonadas;
+};
+
+export const filtrarPaisesLimitantes = (data, paisElegido) => {
+  const paisesLimitantes = [];
+  const pais = data.countries.find(pais => pais.name.common === paisElegido); // Buscamos el país que el usuario eligió
+    if (pais) { // Si encontramos el país, buscamos sus países vecinos
+    pais.borders.forEach(border => {
+      const paisVecino = data.countries.find(pais => pais.fifa=== border);
+    if (paisVecino) {
+        paisesLimitantes.push(paisVecino);
+      }
+    });
+  }
+
+  return paisesLimitantes;
+};
 
--- src/main.js
+++ src/main.js
@@ -1,37 +1,74 @@
-import { example } from './data.js';
+import { example, filtrarIdioma, ordenarAZ, filtrarPaisesLimitantes, calcularArea } from './data.js'; //aqui se agrego ordenar de AZ para darle funcionalidad al boton desde data.js
+//import { example, filtrarDataLimites} from './data.js';
+
 import data from './data/countries/countries.js';
 
 console.log(example, data);
+ 
+//Capturar infirmacion//
+// Mostrar en Lista desplegable la información
+const opcionSeleccionada = document.getElementById("opciones"); 
+
+opcionSeleccionada.addEventListener("change",function(){
+
+  const valorSeleccionado= opcionSeleccionada.value;
+  if(valorSeleccionado==="limitantes"){
+    document.getElementById("limitesForm").style.display="block";
+    document.getElementById("areaForm").style.display="none";
+    document.getElementById("idiomaForm").style.display="none";
+  
+  }
+  else if(valorSeleccionado==="idioma"){
+    document.getElementById("idiomaForm").style.display="block";
+    document.getElementById("areaForm").style.display="none";
+    document.getElementById("limitesForm").style.display="none";
+
+    function volverInicio(){
+      document.getElementById('opciones').style.display = "block";
+      document.getElementById('idiomaForm').style.display = "none"; 
+    }
+  }
+  else if(valorSeleccionado==="calculo"){
+    document.getElementById("areaForm").style.display="block";
+    document.getElementById("idiomaForm").style.display="none";
+    document.getElementById("limitesForm").style.display="none";
+
+    function volverInicio(){
+      document.getElementById('opciones').style.display = "block";
+      document.getElementById('areaForm').style.display = "none"; 
+    }
+  }
+  
+})
 
 // me trae de la data nombre y banderas
 const root = document.getElementById('root');
 
+//movimiento de las banderas  y  informacion trasera de las bandera 
 
-//movimiento de las banderas 
-
-let contentRootInfo = '';
-for (let i = 0; i < data.countries.length; i++) {
-  if (data.countries[i].subregion === 'South America') {
-      const country = data.countries[i];
-      const languages = Object.values(country.languages).join(', ');
-
-      contentRootInfo += `<div class="bandera-container">
-          <div class="bandera-wrapper">
-              <img src="${country.flags.png}" class="Bandera" id="bandera-${i}">
-              <h4>${country.name.common}</h4> 
-          </div>
-          <div class="info-back">
-              <h4>${country.name.common}</h4>
-              <p>Población: ${country.population}</p>
-              <p>Área: ${country.area}</p>
-              <p>Países Frontera: ${country.borders}</p>
-              <p>Idiomas: ${languages}</p>
-          </div>
-      </div>`;
+function pintarData (data){
+  let contentRootInfo = '';
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].subregion === 'South America') {
+        const country = data[i];
+        const languages = Object.values(country.languages).join(', ');
+  
+        contentRootInfo += `<div class="bandera-container">
+            <div class="bandera-wrapper">
+                <img src="${country.flags.png}" class="Bandera" id="bandera-${i}"/>
+                <h4>${country.name.common}</h4> 
+            </div>
+            <div class="info-back">
+                <h4>${country.name.common}</h4>
+                <p>Población: ${country.population}</p>
+                <p>Área: ${country.area}</p>
+                <p>Países Frontera: ${country.borders}</p>
+                <p>Idiomas: ${languages}</p>
+            </div>
+        </div>`;
+    }
   }
-}
-
-root.innerHTML=contentRootInfo;
+  root.innerHTML=contentRootInfo;
 
 const banderaImages = document.querySelectorAll('.Bandera');
 
@@ -49,74 +86,51 @@ banderaImages.forEach(image => {
     image.classList.remove('rotate');
   });
 });
+}
+pintarData(data.countries)
+
+// Botton 1 Organizar
+
+
+const buttonOrdenar = document.getElementById('button1'); // Obtén una referencia al botón de ordenar
+buttonOrdenar.addEventListener('click', () => { // Agrega un evento de clic al botón
+  const banderasOrdenadas = ordenarAZ(root.querySelectorAll('.bandera-container')); // Llama a la función ordenarAZ para ordenar las banderas de A a Z
+  root.innerHTML = '';// Elimina las banderas originales
+  banderasOrdenadas.forEach(bandera => { // Agrega las banderas ordenadas al root nuevamente
+    root.appendChild(bandera);
+  });
+});
+
 
 //Boton de filtros paises limitantes.
 
 // Debemos llamar las funciones que vamos a uctilizar 
-const  btnLimites = document.getElementById('limitantes'); // El boton 
 
-btnLimites.addEventListener("click", function () { // Activo la funcion del botones
-  const inputPais = document.getElementById('input'); // Traigo el inpu que se creo para colocar el nombre 
-  let banderasLimitantes = ''; // Variable con cadena de texto vacia 
-  for (let i= 0; i<data.countries.length; i++) { // Este for es para que me busque la palabra que coloque en la data
-    if(data.countries[i].name.common== inputPais.value) { //Se hace el recorrido cuando sea el mismo nombre lo debe traer 
-      let borders = data.countries[i].borders // Creo una variable que sea igual a la data segun lo que se requiera 
-
-      
-      for (let j= 0; j<borders.length; j++) {  // Este for me trae los bordes paises limites de cada pais 
-        for (let k= 0; k<data.countries.length; k++) {  // Este for me trae la imagen de la badera con el nombre 
-          if(borders[j] === data.countries[k].fifa) { // si los bordes son iguales a lo que tengo en la data me traiga imagenes
-              banderasLimitantes += `<div class="bandera-wrapper"> 
-              <img src="${data.countries[k].flags.png}" class="Bandera" 
-              <h4>${data.countries[k].name.common}</h4> 
-             </div>`;   
-          }
-        }
-      }
-    }
-  }
-  root.innerHTML = banderasLimitantes;
-  });
+const btnLimites = document.getElementById('limitantes');
+btnLimites.addEventListener('click', function () {
+  const inputPais = document.getElementById('input1');
+  const paisElegido = inputPais.value;
+  
+  
+  const paisesLimitantes = filtrarPaisesLimitantes(data, paisElegido);
+  pintarData(paisesLimitantes);
+});
 
 // Boton de filtro Idioma 
 
   const btnIdioma = document.getElementById('idioma')
-  let banderasByIdioma = '';
-
   btnIdioma.addEventListener('click',function () {
-    const inputIdioma = document.getElementById('input');
-    for (let i=0; i<data.countries.length; i++){
-      let lenguajes = data.countries[i].languages;
-      for(const key in lenguajes) {
-        if(lenguajes[key]=== inputIdioma.value) {
-          banderasByIdioma += `<div class="bandera-wrapper"> 
-              <img src="${data.countries[i].flags.png}" class="Bandera" 
-              <h4>${data.countries[i].name.common}</h4> 
-             </div>`;
-        }
-      }    
-    }
-    root.innerHTML = banderasByIdioma;
+    const inputIdioma = document.getElementById('input2').value;
+    const dataFiltradaPorIdioma = filtrarIdioma(data.countries, inputIdioma);
+    pintarData(dataFiltradaPorIdioma);
+  }) 
+
+//calcular área entre dos paises
+  const btnCalcular = document.getElementById ('calcular')
+  btnCalcular.addEventListener('click',function(){
+  const inpuCalcular1 = document.getElementById('area1').value;
+  const inpuCalcular2 = document.getElementById('area2').value;
+  const sumaArea= calcularArea(data, inpuCalcular1,inpuCalcular2);
+  console.log(sumaArea)
+  alert('Area Total: '+ sumaArea)
   })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
