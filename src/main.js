2c4d81f61cf4490033b3ae0a35f1704b54cf400e
Ysmari <mendoza.ysmari@gmail.com>
2023-08-15 15:47:39 -0400

    mejoras en css

--- src/main.js
+++ src/main.js
@@ -9,10 +9,35 @@ const root = document.getElementById('root');
 
 let contentRoot = '';
 for (let i= 0; i<data.countries.length; i++) {  
-    if(data.countries[i].subregion === "South America") {
-        contentRoot += `<img src="${data.countries[i].flags.png}">`;    
-    }
+
+  if (data.countries[i].subregion === "South America") {
+    contentRoot += `<img src="${data.countries[i].flags.png}" class="Bandera">`; }
+    
 }
 root.innerHTML=contentRoot;
+
+const buttons = document.querySelectorAll('aside button');
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function changeButtonColor() {
+  buttons.forEach(button => {
+    const randomColor = getRandomColor();
+    button.style.backgroundColor = randomColor;
+  });
+}
+
+// Cambiar el color de los botones automáticamente cada 2 segundos
+setInterval(changeButtonColor, 2000);
+
+
+
 
 
